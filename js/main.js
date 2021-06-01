388694921be3aa967e4192c70eff20e3236667d2
happysenpai <32872863+happysenpai@users.noreply.github.com>
2021-06-02 00:28:01 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -1,19 +1,18 @@
 const randomNumber = function (min, max) {
-  if (max <= min) {
-    return false;
+  if (max <= min || min < 0 || max < 0) {
+    // eslint-disable-next-line no-console
+    console.log('Не правильное значение');
   }
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-
-randomNumber();
-
 const randomFloat = function (min, max) {
-  if (max <= min) {
-    return false;
+  if (max <= min || min < 0 || max < 0) {
+    // eslint-disable-next-line no-console
+    console.log('Не правильное значение');
   }
   return Math.random() * (max - min) + min;
 };
 
-
+randomNumber();
 randomFloat();
