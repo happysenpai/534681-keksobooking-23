903e4f2118a8833a0cf9ac77904a2a3098d912ce
happysenpai <32872863+happysenpai@users.noreply.github.com>
2021-06-02 00:30:37 +0300

    Update main.js
    fix

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const randomNumber = function (min, max) {
   if (max <= min || min < 0 || max < 0) {
     // eslint-disable-next-line no-console
-    console.log('Не правильное значение');
+    console.log('Неправильное значение');
   }
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
@@ -9,7 +9,7 @@ const randomNumber = function (min, max) {
 const randomFloat = function (min, max) {
   if (max <= min || min < 0 || max < 0) {
     // eslint-disable-next-line no-console
-    console.log('Не правильное значение');
+    console.log('Неправильное значение');
   }
   return Math.random() * (max - min) + min;
 };
