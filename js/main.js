8617f731c4f38037b2c23d9a2203d4280db6789b
happysenpai <32872863+happysenpai@users.noreply.github.com>
2021-06-15 00:05:57 +0300

    Update main.js
    fix

--- js/main.js
+++ js/main.js
@@ -52,7 +52,7 @@ const HOUSE_CARD = {
     lng: randomCoordinates(139.70000, 139.80000, 5),
   },
 };
-
+const RANDOM_AVATAR = randomNumber(1, 8);
 const RANDOM_TITLE = ['Супердом', 'такого вы еше не видели', '1 на миллион'];
 const RANDOM_TYPE = ['flat', 'house', 'bungalow', 'hotel'];
 const RANDOM_CHECKIN = ['12:00', '13:00', '14:00'];
@@ -60,8 +60,8 @@ const RANDOM_CHECKOUT = ['12:00', '13:00', '14:00'];
 const RANDOM_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 const RANDOM_DESCRIPTION = ['лучший дом с садом', 'дом вашей мечты', 'прекрасный вид на море'];
 const RANDOM_PHOTOS = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
-'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
-'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
 const titleIndex = randomNumber(0, RANDOM_TITLE.length - 1);
 const typeIndex = randomNumber(0, RANDOM_TYPE.length - 1);
 const checkinIndex = randomNumber(0, RANDOM_CHECKIN.length - 1);
@@ -72,9 +72,9 @@ const photosIndex = randomNumber(0, RANDOM_PHOTOS.length - 1);
 
 
 const createHouse = () => ({
-  avatar: (HOUSE_CARD.author.avatar),
+  avatar: `img/avatars/user${  0  }${RANDOM_AVATAR   }.png`,
   title: RANDOM_TITLE[titleIndex],
-  address: (HOUSE_CARD.location.lat) + ' ' + (HOUSE_CARD.location.lng),
+  address: `${HOUSE_CARD.location.lat } ${ HOUSE_CARD.location.lng}`,
   price: (HOUSE_CARD.offer.price),
   type: RANDOM_TYPE[typeIndex],
   rooms: (HOUSE_CARD.offer.rooms),
