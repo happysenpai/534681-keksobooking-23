793f20124e145a5ad02ea30c0c79cd30be4f1d63
happysenpai <32872863+happysenpai@users.noreply.github.com>
2021-06-16 23:25:53 +0300

    Update main.js
    fix

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-import {OFFERS_COUNT} from './data.js';
 import {createOffer} from './data.js';
+
+const OFFERS_COUNT = 10;
 
 const offers = new Array(OFFERS_COUNT).fill().map(() => createOffer());
 window.console.log(offers);
