6fa45c6e7dff7327460b352ed7c7185c398e9444
happysenpai <32872863+happysenpai@users.noreply.github.com>
2021-06-16 21:33:21 +0300

    Update main.js
    fix

--- js/main.js
+++ js/main.js
@@ -71,9 +71,9 @@ const houseOffer = () => {
   const coordinatesLat = randomCoordinates(MIN_COORDINATES_LAT, MAX_COORDINATES_LAT, COORDINAT_FLOAT_COUNT);
   const coordinatesLng = randomCoordinates(MIN_COORDINATES_LNG, MAX_COORDINATES_LNG, COORDINAT_FLOAT_COUNT);
   return{
-    avatar: `img/avatars/user${  0  }${avatarValue}.png`,
+    avatar: `img/avatars/user${0}${avatarValue}.png`,
     title: TITLES[titleIndex],
-    address: `${coordinatesLat } ${  coordinatesLng}`,
+    address: `${coordinatesLat} ${coordinatesLng}`,
     price: priceValue,
     type: TYPES[typeIndex],
     rooms: roomsValue,
