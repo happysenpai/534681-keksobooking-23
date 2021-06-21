4b79c86c99e3edb824ea6ed34b1970c399be575d
happysenpai <32872863+happysenpai@users.noreply.github.com>
2021-06-21 15:53:51 +0300

    Update main.js
    fix

--- js/main.js
+++ js/main.js
@@ -3,4 +3,10 @@ import {createOffer} from './data.js';
 const OFFERS_COUNT = 10;
 
 const offers = new Array(OFFERS_COUNT).fill().map(() => createOffer());
-window.console.log(offers);
+
+const cardTitle = offers[0].offer.title;
+
+const cardAdress = offers[0].offer.address;
+
+
+export {cardTitle,cardAdress};
