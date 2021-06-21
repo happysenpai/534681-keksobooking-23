ff9dc451b79ed0915282dca78cc7a3e4f916808e
happysenpai <32872863+happysenpai@users.noreply.github.com>
2021-06-21 14:40:00 +0300

    Update dom_gen.js
    fix

--- js/dom_gen.js
+++ js/dom_gen.js
@@ -1,15 +1,13 @@
-import {createOffer} from './data.js';
+//import {createOffer} from './data.js';
 const mainBock = document.querySelector('#map-canvas');
 const baseTemplate = document.querySelector('#card').content;
 
-const cloneTemplate = template.cloneNode(true);
+const cloneTemplate = baseTemplate.cloneNode(true);
 
-const newTitle = baseTemplate.querySelector('h3').textContent;
+const newTitle = cloneTemplate.querySelector('h3').textContent;
 baseTemplate.querySelector('h3').textContent = 'текст поменял';
 
 window.console.log(newTitle);
 
 
-
 mainBock.appendChild(baseTemplate);
- 
