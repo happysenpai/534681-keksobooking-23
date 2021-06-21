98e6747e5e64de968333eb1545a39d62fcd06352
happysenpai <32872863+happysenpai@users.noreply.github.com>
2021-06-21 15:53:56 +0300

    Update popup.js
    fix

--- js/popup.js
+++ js/popup.js
@@ -1,13 +1,16 @@
-//import {createOffer} from './data.js';
+import {cardTitle,cardAdress} from './main.js';
 const mainBlock = document.querySelector('#map-canvas');
 const baseTemplate = document.querySelector('#card').content;
 
 const cloneTemplate = baseTemplate.cloneNode(true);
 
-const newTitle = cloneTemplate.querySelector('h3').textContent;
-baseTemplate.querySelector('h3').textContent = 'текст поменял';
 
-window.console.log(newTitle);
+const newTitle = cloneTemplate.querySelector('.popup__title').textContent;
+baseTemplate.querySelector('.popup__title').textContent = cardTitle;
 
+const newAdress = cloneTemplate.querySelector('.popup__text--address').textContent;
+baseTemplate.querySelector('.popup__text--address').textContent = cardAdress;
 
+window.console.log(newTitle,newAdress);
 mainBlock.appendChild(baseTemplate);
+
