a2aa67fbc26c381b818ab135f49de104f6eda915
happysenpai <32872863+happysenpai@users.noreply.github.com>
2021-06-21 16:08:46 +0300

    Update popup.js
    fix

--- js/popup.js
+++ js/popup.js
@@ -2,15 +2,19 @@ import {cardTitle,cardAdress} from './main.js';
 const mainBlock = document.querySelector('#map-canvas');
 const baseTemplate = document.querySelector('#card').content;
 
-const cloneTemplate = baseTemplate.cloneNode(true);
+const fragment = () => {
 
+  const cloneTemplate = baseTemplate.cloneNode(true);
 
-const newTitle = cloneTemplate.querySelector('.popup__title').textContent;
-baseTemplate.querySelector('.popup__title').textContent = cardTitle;
+  const newTitle = cloneTemplate.querySelector('.popup__title').textContent;
+  baseTemplate.querySelector('.popup__title').textContent = cardTitle;
 
-const newAdress = cloneTemplate.querySelector('.popup__text--address').textContent;
-baseTemplate.querySelector('.popup__text--address').textContent = cardAdress;
+  const newAdress = cloneTemplate.querySelector('.popup__text--address').textContent;
+  baseTemplate.querySelector('.popup__text--address').textContent = cardAdress;
 
-window.console.log(newTitle,newAdress);
-mainBlock.appendChild(baseTemplate);
+  window.console.log(newTitle,newAdress);
+  return mainBlock.appendChild(baseTemplate);
+};
+fragment();
+
 
