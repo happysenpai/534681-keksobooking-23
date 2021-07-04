d8e25f8bc0e36d8f5631d52e8c4d606b5a3a6f31
happysenpai <32872863+happysenpai@users.noreply.github.com>
2021-07-04 20:05:24 +0300

    fix

---
+++ js/user-form.js
@@ -0,0 +1,30 @@
+const MIN_NAME_LENGTH = 30;
+const MAX_NAME_LENGTH = 100;
+const MAX_PRICE = 1000000;
+const userTitleInput = document.querySelector('#title');
+const userPriceInput = document.querySelector('#price');
+
+userTitleInput.addEventListener('input', () => {
+  const valueLength = userTitleInput.value.length;
+
+  if (valueLength < MIN_NAME_LENGTH) {
+    userTitleInput.setCustomValidity(`Ещё ${  MIN_NAME_LENGTH - valueLength } симв.`);
+  } else if (valueLength > MAX_NAME_LENGTH) {
+    userTitleInput.setCustomValidity(`Удалите лишние ${  valueLength - MAX_NAME_LENGTH } симв.`);
+  } else {
+    userTitleInput.setCustomValidity('');
+  }
+
+  userTitleInput.reportValidity();
+});
+
+userPriceInput.addEventListener('input', () => {
+  const valueLength = userPriceInput.value;
+  if (valueLength > MAX_PRICE) {
+    userPriceInput.setCustomValidity(`Удалите лишние ${  valueLength - MAX_PRICE } симв.`);
+  } else {
+    userPriceInput.setCustomValidity('');
+  }
+
+  userPriceInput.reportValidity();
+});
